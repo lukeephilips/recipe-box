914d6dc22dcf8c05f95c75eeef1edc9f32a65fdb
Luke <luke.e.philips@gmail.com>
2017-01-15 22:07:03 -0800

    add skeleton

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>Baby's First Angular 2 App</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+}
