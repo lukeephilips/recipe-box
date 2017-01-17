f30f557f68a3217cdaa74ebf5d176181ff9aca23
Luke <luke.e.philips@gmail.com>
2017-01-17 12:18:38 -0800

    constructor working without public parameters

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>Baby's First Angular 2 App</h1>
+    <h1>Granny's Recipe Box</h1>
+
+    <h2>{{testRecipe.title}}</h2>
+    <p>{{testRecipe.directions}}</p>
+    <p>{{testRecipe.ingredients}}</p>
+
   </div>
   `
 })
 
 export class AppComponent {
+  testRecipe = new Recipe('toast', 'put bread in toaster', ['a','b']);
+}
+
+export class Recipe {
+  title: string;
+  directions: string;
+  ingredients: string[];
+
+  constructor(titleP: string, directionsP: string, ingredientsP: string[]) {
+    this.title = titleP;
+    this.directions = directionsP;
+    this.ingredients = ingredientsP;
+  }
+
 }
