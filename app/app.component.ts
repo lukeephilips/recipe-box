39c6e3a6054d30d4656a9419fc962f4d0c95ed3f
Luke <luke.e.philips@gmail.com>
2017-01-17 13:09:40 -0800

    complete monday assignment, add materialize

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,29 +4,40 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>Granny's Recipe Box</h1>
-
-    <h2>{{testRecipe.title}}</h2>
-    <p>{{testRecipe.directions}}</p>
-    <p>{{testRecipe.ingredients}}</p>
-
+    <h1 class="center">Granny's Recipe Box</h1>
+      <div *ngFor="let recipe of recipes" class=" recipe card">
+        <div class="card-image">
+          <img src={{recipe.image}}/>
+          <span class="card-title">{{recipe.title}}</span>
+        </div>
+        <div class="card-content">
+          <div class="col s6">
+            <h4>Ingredients</h4>
+            <ul>
+              <li *ngFor="let ingredient of recipe.ingredients">{{ingredient}}</li>
+            </ul>
+          </div>
+          <div class="col s6">
+            <h4>Directions</h4>
+            <ol>
+              <li *ngFor="let direction of recipe.directions">{{direction}}</li>
+            </ol>
+          </div>
+        </div>
+      </div>
   </div>
   `
 })
 
 export class AppComponent {
-  testRecipe = new Recipe('toast', 'put bread in toaster', ['a','b']);
+  recipes: Recipe[] = [
+    new Recipe('toast', ['put bread in toaster','put butter on toast'], ['bread','butter'],'http://images.amcnetworks.com/bbcamerica.com/wp-content/uploads/2013/06/Toast.jpg'),
+    new Recipe('tea', ['boil water','put teabag in water'], ['teabag','water'],'http://kingofwallpapers.com/tea/tea-044.jpg'),
+    new Recipe('orange slices', ['slice orange'], ['one orange'],'http://files.gamebanana.com/img/ico/sprays/4ff6e9ef24135.png'),
+  ]
 }
 
 export class Recipe {
-  title: string;
-  directions: string;
-  ingredients: string[];
 
-  constructor(titleP: string, directionsP: string, ingredientsP: string[]) {
-    this.title = titleP;
-    this.directions = directionsP;
-    this.ingredients = ingredientsP;
-  }
-
+  constructor(public title: string, public directions: string[], public ingredients: string[], public image: string) { }
 }
